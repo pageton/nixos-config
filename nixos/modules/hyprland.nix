2a05550ec6c285ad2ded21086f562ea84ebe4e2a
Sadiq <pageton@proton.me>
2025-08-05 16:28:03 +0300

    feat(hyprland): enable polkit and PAM services for hyprlock and keyring

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -4,11 +4,18 @@
   programs.hyprland = {
     enable = true;
     withUWSM = true;
-    xwayland.enable = true;
-
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
 
     portalPackage =
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+
+  # Enable security services
+  security = {
+    polkit.enable = true;
+    pam.services = {
+      hyprlock = { };
+      gdm.enableGnomeKeyring = true;
+    };
   };
 }
