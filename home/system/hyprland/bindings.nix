2feb3ef80b3024fed187f43cfc72430d5da35e5e
Sadiq <pageton@proton.me>
2025-08-05 16:20:09 +0300

    refactor(hyprland): simplify keybindings list formatting

--- home/system/hyprland/bindings.nix
+++ home/system/hyprland/bindings.nix
@@ -1,49 +1,49 @@
 { pkgs, ... }:
 {
   wayland.windowManager.hyprland.settings = {
-    bind =
-      [
-        "$mod,T, exec, uwsm app -- ${pkgs.kitty}/bin/kitty"
-        "$mod,B, exec,  uwsm app -- brave"
-        "$shiftMod,L, exec,  uwsm app -- ${pkgs.hyprlock}/bin/hyprlock"
-        "$mod,X, exec, powermenu"
-        "$mod,SPACE, exec, menu"
-        "$mod,C, exec, quickmenu"
-        "$shiftMod,SPACE, exec, hyprfocus-toggle"
-        "$mod, E, exec, ${pkgs.kitty}/bin/kitty -e yazi"
-        "$mod,Q, killactive,"
-        "$mod,W, togglefloating,"
-        "$mod,F, fullscreen"
-        "$mod,h, movefocus, l"
-        "$mod,l, movefocus, r"
-        "$mod,k, movefocus, u"
-        "$mod,j, movefocus, d"
-        "$shiftMod,up, focusmonitor, -1"
-        "$shiftMod,down, focusmonitor, 1"
-        "$shiftMod,left, layoutmsg, addmaster"
-        "$shiftMod,right, layoutmsg, removemaster"
-        "$mod,P, exec, hyprshot -m window -o ~/Pictures/screenshots"
-        "$shiftMod,P, exec, hyprshot -m region -o ~/Pictures/screenshots"
-        "$mod,S, togglespecialworkspace, magic"
-        "$shiftMod,S, movetoworkspace, special:magic"
-        "$shiftMod,T, exec, hyprpanel-toggle"
-        "$mod,V, exec, clipboard"
-        "$shiftMod,E, exec, ${pkgs.wofi-emoji}/bin/wofi-emoji"
-        "$mod,F2, exec, night-shift"
-        "$mod,F3, exec, night-shift"
-      ]
-      ++ (builtins.concatLists (
-        builtins.genList (
-          i:
-          let
-            ws = i + 1;
-          in
-          [
-            "$mod,code:1${toString i}, workspace, ${toString ws}"
-            "$mod SHIFT,code:1${toString i}, movetoworkspace, ${toString ws}"
-          ]
-        ) 9
-      ));
+    bind = [
+      "$mod,T, exec, uwsm app -- ${pkgs.kitty}/bin/kitty"
+      "$mod,B, exec,  uwsm app -- brave"
+      "$shiftMod,L, exec,  uwsm app -- ${pkgs.hyprlock}/bin/hyprlock"
+      "$mod,X, exec, powermenu"
+      "$mod,SPACE, exec, menu"
+      "$mod,C, exec, quickmenu"
+      "$shiftMod,SPACE, exec, hyprfocus-toggle"
+      "$mod, E, exec, ${pkgs.kitty}/bin/kitty -e yazi"
+      "$shiftMod, E, exec, nautilus"
+      "$mod,Q, killactive,"
+      "$mod,W, togglefloating,"
+      "$mod,F, fullscreen"
+      "$mod,h, movefocus, l"
+      "$mod,l, movefocus, r"
+      "$mod,k, movefocus, u"
+      "$mod,j, movefocus, d"
+      "$shiftMod,up, focusmonitor, -1"
+      "$shiftMod,down, focusmonitor, 1"
+      "$shiftMod,left, layoutmsg, addmaster"
+      "$shiftMod,right, layoutmsg, removemaster"
+      "$mod,P, exec, hyprshot -m window -o ~/Pictures/screenshots"
+      "$shiftMod,P, exec, hyprshot -m region -o ~/Pictures/screenshots"
+      "$mod,S, togglespecialworkspace, magic"
+      "$shiftMod,S, movetoworkspace, special:magic"
+      "$shiftMod,T, exec, hyprpanel-toggle"
+      "$mod,V, exec, clipboard"
+      "$shiftMod,E, exec, ${pkgs.wofi-emoji}/bin/wofi-emoji"
+      "$mod,F2, exec, night-shift"
+      "$mod,F3, exec, night-shift"
+    ]
+    ++ (builtins.concatLists (
+      builtins.genList (
+        i:
+        let
+          ws = i + 1;
+        in
+        [
+          "$mod,code:1${toString i}, workspace, ${toString ws}"
+          "$mod SHIFT,code:1${toString i}, movetoworkspace, ${toString ws}"
+        ]
+      ) 9
+    ));
 
     bindm = [
       "$mod,mouse:272, movewindow"
