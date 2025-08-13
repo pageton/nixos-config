43f0dc1600c6c74127e4c8a98b79b98abb37c003
Sadiq <pageton@proton.me>
2025-08-13 14:58:55 +0300

    chore(sddm): minor configuration improvement

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -1,56 +1,69 @@
-{
-  pkgsStable,
-  inputs,
-  config,
-  ...
+# SDDM configuration (login screen).
+
+{ pkgs
+, inputs
+, config
+, lib
+, ...
 }:
 let
   foreground = config.theme.textColorOnWallpaper;
-  sddm-astronaut = pkgsStable.sddm-astronaut.override {
+  sddm-astronaut = pkgs.sddm-astronaut.override {
     embeddedTheme = "pixel_sakura";
-    themeConfig = {
-      HeaderTextColor = "#${foreground}";
-      DateTextColor = "#${foreground}";
-      TimeTextColor = "#${foreground}";
-      LoginFieldTextColor = "#${foreground}";
-      PasswordFieldTextColor = "#${foreground}";
-      UserIconColor = "#${foreground}";
-      PasswordIconColor = "#${foreground}";
-      WarningColor = "#${foreground}";
-      LoginButtonBackgroundColor = "#${foreground}";
-      SystemButtonsIconsColor = "#${foreground}";
-      SessionButtonTextColor = "#${foreground}";
-      VirtualKeyboardButtonTextColor = "#${foreground}";
-      DropdownBackgroundColor = "#${foreground}";
-      HighlightBackgroundColor = "#${foreground}";
-      Background =
-        if "sakura_pixelart_light_static.png" == config.stylix.image then
-          pkgsStable.fetchurl {
-            url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/sakura_pixelart_light_animated.gif";
+    themeConfig =
+      if lib.hasSuffix "sakura_static.png" config.stylix.image then
+        { }
+      else if lib.hasSuffix "studio.png" config.stylix.image then
+        {
+          Background = pkgs.fetchurl {
+            url = "https://raw.githubusercontent.com/anotherhadi/nixy-wallpapers/refs/heads/main/wallpapers/studio.gif";
             sha256 = "sha256-qySDskjmFYt+ncslpbz0BfXiWm4hmFf5GPWF2NlTVB8=";
-          }
-        else if "cat-watching-the-star_pixelart_purple_static.png" == config.stylix.image then
-          pkgsStable.fetchurl {
-            url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/cat-watching-the-star_pixelart_purple_animated.gif";
-            sha256 = "";
-          }
-        else
-          "${toString config.stylix.image}";
-    };
+          };
+          HeaderTextColor = "#${foreground}";
+          DateTextColor = "#${foreground}";
+          TimeTextColor = "#${foreground}";
+          LoginFieldTextColor = "#${foreground}";
+          PasswordFieldTextColor = "#${foreground}";
+          UserIconColor = "#${foreground}";
+          PasswordIconColor = "#${foreground}";
+          WarningColor = "#${foreground}";
+          LoginButtonBackgroundColor = "#${foreground}";
+          SystemButtonsIconsColor = "#${foreground}";
+          SessionButtonTextColor = "#${foreground}";
+          VirtualKeyboardButtonTextColor = "#${foreground}";
+          DropdownBackgroundColor = "#${foreground}";
+          HighlightBackgroundColor = "#${foreground}";
+        }
+      else
+        {
+          Background = "${toString config.stylix.image}";
+          HeaderTextColor = "#${foreground}";
+          DateTextColor = "#${foreground}";
+          TimeTextColor = "#${foreground}";
+          LoginFieldTextColor = "#${foreground}";
+          PasswordFieldTextColor = "#${foreground}";
+          UserIconColor = "#${foreground}";
+          PasswordIconColor = "#${foreground}";
+          WarningColor = "#${foreground}";
+          LoginButtonBackgroundColor = "#${foreground}";
+          SystemButtonsIconsColor = "#${foreground}";
+          SessionButtonTextColor = "#${foreground}";
+          VirtualKeyboardButtonTextColor = "#${foreground}";
+          DropdownBackgroundColor = "#${foreground}";
+          HighlightBackgroundColor = "#${foreground}";
+        };
   };
 in
 {
   services.displayManager = {
     sddm = {
-      package = pkgsStable.kdePackages.sddm;
+      package = pkgs.kdePackages.sddm;
       extraPackages = [ sddm-astronaut ];
       enable = true;
       wayland.enable = true;
       theme = "sddm-astronaut-theme";
       settings = {
-        Wayland.SessionDir = "${
-          inputs.hyprland.packages."${pkgsStable.system}".hyprland
-        }/share/wayland-sessions";
+        Wayland.SessionDir = "${inputs.hyprland.packages."${pkgs.system}".hyprland}/share/wayland-sessions";
       };
     };
   };
