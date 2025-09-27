3eb148170ec15727b343e9e1d77dbfca9fd4df1a
Sadiq <pageton@proton.me>
2025-09-27 08:05:21 +0300

    feat(packages): add vscode and chromium

--- home/packages/applications.nix
+++ home/packages/applications.nix
@@ -2,12 +2,14 @@
 
 with pkgs;
 [
-  tor-browser
-  brave
-  telegram-desktop
-  obs-studio
-  obsidian
-  vlc
-  nautilus
-  vesktop
+  tor-browser # Tor browser
+  brave # Brave browser
+  telegram-desktop # Telegram desktop
+  obs-studio # OBS Studio
+  obsidian # Obsidian
+  vlc # VLC media player
+  nautilus # Nautilus file manager
+  vesktop # Discord desktop client
+  vscode # Visual Studio Code
+  chromium # Chromium web browser
 ]
