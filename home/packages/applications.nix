e39a8a443a9aaecd641f50895f9439ac7dca6f98
Sadiq <pageton@proton.me>
2025-10-26 16:32:56 +0300

    chore(home): simplify applications.nix configuration

--- home/packages/applications.nix
+++ home/packages/applications.nix
@@ -5,18 +5,16 @@ with pkgs;
   tor-browser # Tor browser
   brave # Brave browser
   telegram-desktop # Telegram desktop
-  obs-studio # OBS Studio
-  # obsidian # Obsidian
+  obsidian # Obsidian
   vlc # VLC media player
   vesktop # Discord desktop client
   vscode # Visual Studio Code
-  chromium # Chromium web browser
   # Gaming and compatibility tools
   (bottles.override { removeWarningPopup = true; }) # Run Windows applications on Linux
   rustdesk # Remote desktop software
   anydesk # Remote desktop software
   wineWowPackages.stagingFull
-  winetricks
   samba
   kdePackages.dolphin # KDE file manager
+  materialgram
 ]
