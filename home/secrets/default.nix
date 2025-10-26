bf9866d41406682d2eba850b2efc78ea523211b1
Sadiq <pageton@proton.me>
2025-10-26 16:37:40 +0300

    chore(secrets): reorganize secrets configuration

--- home/secrets/default.nix
+++ home/secrets/default.nix
@@ -1,48 +1,49 @@
-{ inputs, user, ... }:
+{
+  inputs,
+  user,
+  pkgs,
+  ...
+}:
 
 {
   imports = [ inputs.sops-nix.homeManagerModules.sops ];
 
   sops = {
-    age.keyFile = "/home/${user}/.config/sops/age/keys.txt";
-    defaultSopsFile = ./secrets.yaml;
+    defaultSopsFormat = "yaml"; # Format of the secrets file
+    validateSopsFiles = true; # Enable SOPS file validation
+    defaultSopsFile = ./secrets.yaml; # Path to encrypted secrets file (relative to this module)
+
+    age = {
+      keyFile = "/home/${user}/.config/sops/age/keys.txt"; # Location of Age private key
+      generateKey = false;
+    };
 
     secrets = {
       gpg-private-key = {
         path = "/home/${user}/.gnupg/private.key";
-        mode = "0600";
       };
       gpg-public-key = {
         path = "/home/${user}/.gnupg/public.key";
-        mode = "0644";
       };
       signing-key = {
         path = "/home/${user}/.ssh/id_rsa";
-        mode = "0600";
       };
       signing-pub-key = {
         path = "/home/${user}/.ssh/id_rsa.pub";
-        mode = "0644";
       };
       zai-api-key = {
-        mode = "0644";
+        path = "/home/${user}/.config/zai";
       };
 
     };
   };
 
-  home.file."/home/${user}/System/.sops.yaml".text = ''
-    keys:
-      - &primary age14zz7lfqlzx4grk28whm7eu8w4aecxytc9rpuee3qj8yu4yet7cws9law7x
-
-    creation_rules:
-      - path_regex: home/secrets/secrets.yaml$
-        key_groups:
-          - age:
-              - *primary
-  '';
-
   systemd.user.services.mbsync.Unit.After = [ "sops-nix.service" ];
+
+  home.packages = with pkgs; [
+    sops
+    age
+  ];
 
   wayland.windowManager.hyprland.settings.exec-once = [ "systemctl --user start sops-nix" ];
 }
