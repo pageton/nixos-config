b98d6df0f13c2b84a245a77bd4ed49ac1329ea06
Sadiq <pageton@proton.me>
2025-10-28 17:12:46 +0300

    chore(bootloader): switch from systemd-boot to GRUB with UEFI support

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,8 +1,20 @@
-# Bootloader configuration (systemd-boot).
+# Bootloader configuration (GRUB - no theme).
 
 {
   boot.loader = {
-    systemd-boot.enable = true;
+    # Disable systemd-boot
+    systemd-boot.enable = false;
+
+    # Enable GRUB
+    grub = {
+      enable = true;
+      device = "nodev"; # Use UEFI boot
+      efiSupport = true;
+      # For UEFI systems, we need to allow touching EFI variables
+      efiInstallAsRemovable = false;
+    };
+
+    # Allow touching EFI variables for UEFI systems
     efi.canTouchEfiVariables = true;
   };
 }
