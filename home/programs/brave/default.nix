9de99b3486ebff891948eb3ea47275e30cc02734
Sadiq <pageton@proton.me>
2025-11-23 23:15:37 +0300

    feat(browser): add Brave browser configuration with security policies

---
+++ home/programs/brave/default.nix
@@ -0,0 +1,18 @@
+# Brave Browser configuration
+{
+  programs.brave = {
+    enable = true;
+    extensions = [
+      "bfogiafebfohielmmehodmfbbebbbpei" # Keeper Password Manager
+      "ielooaepfhfcnmihgnabkldnpddnnldl" # Multilanguage Translator
+      "mpbjkejclgfgadiemmefgebjfooflfhl" # Buster: Captcha Solver for Humans
+      "amefmmaoenlhckgaoppgnmhlcolehkho" # github-vscode-icons
+      "iphcomljdfghbkdcfndaijbokpgddeno" # Cookie Editor
+      "cclelndahbckbenkjhflpdbgdldlbecc" # Get cookies.txt LOCALLY
+      "hlepfoohegkhhmjieoechaddaejaokhf" # Refined GitHub
+      "gppongmhjkpfnbhagpmjfkannfbllamg" # Wappalyzer - Technology profiler
+      "hfjbmagddngcpeloejdejnfgbamkjaeg" # Vimium C - All by Keyboard
+      "eifflpmocdbdmepbjaopkkhbfmdgijcc" # JSON Viewer Pro
+    ];
+  };
+}
