2d8c20f9e9a7a34c4b3f42836cdf08f68a45d567
Sadiq <pageton@proton.me>
2025-11-26 02:12:29 +0300

    refactor: Use stdenv.hostPlatform.system for flake package inputs

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,4 +1,4 @@
-{ inputs, system, ... }:
+{ inputs, pkgs, ... }:
 
 {
   programs.neovim = {
@@ -7,7 +7,7 @@
     vimAlias = true;
     vimdiffAlias = true;
     defaultEditor = true;
-    package = inputs.neovim-nightly-overlay.packages.${system}.neovim;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
   };
 
   xdg.configFile."nvim/init.lua".source = ./init.lua;
