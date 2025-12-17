d57d3ea01e3ad04d4edc00677a68ced3f0873340
Sadiq <pageton@proton.me>
2025-12-17 19:20:05 +0300

    feat(nix): add vicinae flake input

--- flake.nix
+++ flake.nix
@@ -31,96 +31,101 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    vicinae.url = "github:vicinaehq/vicinae";
+
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-stable,
-      home-manager,
-      ...
-    }@inputs:
-    let
-      system = "x86_64-linux";
-      homeStateVersion = "25.11";
-      user = "sadiq";
-      pkgs = import nixpkgs {
-        inherit system;
-        allowUnfree = true; # Allow proprietary packages
-        allowBroken = true; # Don't allow broken packages
-        allowInsecure = false; # Don't allow insecure packages
-        allowUnsupportedSystem = false; # Don't allow unsupported systems
-      };
-
-      pkgsStable = import nixpkgs-stable {
-        inherit system;
-        allowUnfree = true;
-        allowBroken = false;
-        allowInsecure = false;
-        allowUnsupportedSystem = false;
-      };
-
-      makeSystem =
-        { hostname, stateVersion }:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit
-              inputs
-              stateVersion
-              hostname
-              user
-              pkgsStable
-              ;
-          };
-          modules = [
-            ./hosts/${hostname}/configuration.nix
-            inputs.stylix.nixosModules.stylix
-          ];
-        };
-
-      hosts = [
-        {
-          hostname = "desktop";
-          stateVersion = "25.11";
-        }
-        {
-          hostname = "thinkpad";
-          stateVersion = "25.11";
-        }
-      ];
-    in
-    {
-      nixosConfigurations = nixpkgs.lib.foldl' (
-        configs: host:
-        configs // { "${host.hostname}" = makeSystem { inherit (host) hostname stateVersion; }; }
-      ) { } hosts;
-
-      homeConfigurations = nixpkgs.lib.foldl' (
-        configs: host:
-        configs
-        // {
-          "${user}@${host.hostname}" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs = {
-              inherit
-                inputs
-                homeStateVersion
-                user
-                pkgsStable
-                system
-                ;
-              hostname = host.hostname;
-            };
-
-            modules = [
-              ./home/home.nix
-              inputs.stylix.homeModules.stylix
-            ];
-          };
-        }
-      ) { } hosts;
+  outputs = {
+    # self,
+    nixpkgs,
+    nixpkgs-stable,
+    home-manager,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    homeStateVersion = "25.11";
+    user = "sadiq";
+    pkgs = import nixpkgs {
+      inherit system;
+      allowUnfree = true; # Allow proprietary packages
+      allowBroken = true; # Don't allow broken packages
+      allowInsecure = false; # Don't allow insecure packages
+      allowUnsupportedSystem = false; # Don't allow unsupported systems
     };
+
+    pkgsStable = import nixpkgs-stable {
+      inherit system;
+      allowUnfree = true;
+      allowBroken = false;
+      allowInsecure = false;
+      allowUnsupportedSystem = false;
+    };
+
+    makeSystem = {
+      hostname,
+      stateVersion,
+    }:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit
+            inputs
+            stateVersion
+            hostname
+            user
+            pkgsStable
+            ;
+        };
+        modules = [
+          ./hosts/${hostname}/configuration.nix
+          inputs.stylix.nixosModules.stylix
+        ];
+      };
+
+    hosts = [
+      {
+        hostname = "desktop";
+        stateVersion = "25.11";
+      }
+      {
+        hostname = "thinkpad";
+        stateVersion = "25.11";
+      }
+    ];
+  in {
+    nixosConfigurations =
+      nixpkgs.lib.foldl' (
+        configs: host:
+          configs // {"${host.hostname}" = makeSystem {inherit (host) hostname stateVersion;};}
+      ) {}
+      hosts;
+
+    homeConfigurations =
+      nixpkgs.lib.foldl' (
+        configs: host:
+          configs
+          // {
+            "${user}@${host.hostname}" = home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              extraSpecialArgs = {
+                inherit
+                  inputs
+                  homeStateVersion
+                  user
+                  pkgsStable
+                  system
+                  ;
+                inherit (host) hostname;
+              };
+
+              modules = [
+                ./home/home.nix
+                inputs.stylix.homeModules.stylix
+              ];
+            };
+          }
+      ) {}
+      hosts;
+  };
 }
