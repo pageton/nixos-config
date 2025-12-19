803f312b8c9869ad252b8315650c47b6f728ea3e
Sadiq <pageton@proton.me>
2025-12-19 15:20:40 +0300

    chore(applications): adjust installed desktop applications

--- home/packages/applications.nix
+++ home/packages/applications.nix
@@ -4,14 +4,14 @@ with pkgs; [
   telegram-desktop # Telegram desktop
   obsidian # Obsidian
   vlc # VLC media player
-  vesktop # Discord desktop client
   vscode # Visual Studio Code
   # Gaming and compatibility tools
   (bottles.override {removeWarningPopup = true;}) # Run Windows applications on Linux
   wineWowPackages.stagingFull
   samba
-  kdePackages.dolphin # KDE file manager
   zed-editor
   remmina # RDP client
   antigravity
+  youtube-music # YouTube Music desktop client
+  kdePackages.ark # KDE archive manager
 ]
