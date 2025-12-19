4875a70e5e2005799a023289c6ba5c8c019f20db
Sadiq <pageton@proton.me>
2025-12-19 15:43:40 +0300

    chore(nixos): enable systemd-resolved and add Mullvad to system packages

--- nixos/modules/mullvad.nix
+++ nixos/modules/mullvad.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }:
-{
-  services = {
-    mullvad-vpn = {
-      enable = true;
-      package = pkgs.mullvad-vpn; # Optional
-    };
+# Mullvad VPN configuration.
+{pkgs, ...}: {
+  # Enable systemd-resolved
+  services.resolved.enable = true;
+
+  # Enable Mullvad VPN service
+  services.mullvad-vpn = {
+    enable = true;
+    # GUI package for easy management and desktop integration
+    package = pkgs.mullvad-vpn;
   };
+
+  # Add Mullvad VPN to system packages for CLI access
+  environment.systemPackages = with pkgs; [mullvad-vpn];
 }
