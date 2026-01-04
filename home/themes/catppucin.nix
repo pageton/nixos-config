86f1cdf1bd6526e84923b5682cfbd90f12b4b907
Sadiq <pageton@proton.me>
2026-01-04 23:04:51 +0300

    refactor(catppuccin): change animation timing setting

--- home/themes/catppucin.nix
+++ home/themes/catppucin.nix
@@ -14,7 +14,7 @@
       inactive-opacity = 0.94;
       blur = true;
       border-size = 3;
-      animation-speed = "fast"; # "fast" | "medium" | "slow"
+      animation-speed = "medium"; # "fast" | "medium" | "slow"
       fetch = "none"; # "nerdfetch" | "neofetch" | "pfetch" | "none"
       textColorOnWallpaper = config.lib.stylix.colors.base05; # Color of the text displayed on the wallpaper (Lockscreen, display manager, ...)
 
