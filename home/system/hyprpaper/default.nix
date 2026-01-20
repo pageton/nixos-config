6ed36a25a83a868bc50ad2ac12f00150efdb0bc9
Sadiq <pageton@proton.me>
2026-01-20 13:35:35 +0300

    style(hyprpaper): remove trailing zeros in configuration values

--- home/system/hyprpaper/default.nix
+++ home/system/hyprpaper/default.nix
@@ -1,11 +1,10 @@
-{ lib, ... }:
-{
+{lib, ...}: {
   services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "on";
       splash = false;
-      splash_offset = 2.0;
+      splash_offset = 2;
     };
   };
   systemd.user.services.hyprpaper.Unit.After = lib.mkForce "graphical-session.target";
