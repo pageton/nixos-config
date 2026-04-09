e2ffd7798f7df4a6581cda05a97e970b20932830
Sadiq <pageton@proton.me>
2026-04-09 20:12:41 +0300

    refactor(helix): restructure keybindings into nested attrset
    - Flatten keys.normal/insert/select into single keys attrset
    - Nest space and g subkeymaps properly
    - Move g.d (goto_definition) to g.f (goto_file) in select mode

--- home/programs/helix/default.nix
+++ home/programs/helix/default.nix
@@ -1,8 +1,6 @@
+{ pkgs, ... }:
 {
-  pkgs,
-  ...
-}: {
-  imports = [./languages.nix];
+  imports = [ ./languages.nix ];
   programs.helix = {
     enable = true;
     package = pkgs.helix;
@@ -55,160 +53,164 @@
             "file-encoding"
             "file-type"
           ];
-          mode.normal = "NORMAL";
-          mode.insert = "INSERT";
-          mode.select = "SELECT";
+          mode = {
+            normal = "NORMAL";
+            insert = "INSERT";
+            select = "SELECT";
+          };
         };
       };
-      keys.normal = {
-        space.w = ":write";
-        space.q = ":quit";
-        space.f = ":fmt";
-        space.r = ":config-reload";
-        g.a = "code_action";
+      keys = {
+        normal = {
+          space = {
+            w = ":write";
+            q = ":quit";
+            f = ":fmt";
+            r = ":config-reload";
+            i = ":toggle-option lsp.display-inlay-hints";
+            space = {
+              b = ":sh git blame -L %{cursor_line},%{cursor_line} %{buffer_name}";
+            };
+          };
+          g = {
+            a = "code_action";
+            D = "no_op";
+            d = {
+              c = [ "goto_definition" ];
+              v = [
+                "vsplit"
+                "jump_view_up"
+                "goto_definition"
+              ];
+              h = [
+                "hsplit"
+                "jump_view_up"
+                "goto_definition"
+              ];
+            };
+          };
 
-        V = [
-          "select_mode"
-          "extend_to_line_bounds"
-        ];
-
-        C-k = [
-          "extend_to_line_bounds"
-          "delete_selection"
-          "move_line_up"
-          "paste_clipboard_before"
-        ];
-
-        D = [
-          "extend_to_line_bounds"
-          "delete_selection"
-        ];
-
-        C-j = [
-          "extend_to_line_bounds"
-          "delete_selection"
-          "paste_clipboard_after"
-        ];
-        A-q = [":reflow"];
-        C-y = ["scroll_up"];
-        C-e = ["scroll_down"];
-
-        p = [
-          "delete_selection"
-          "paste_clipboard_before"
-        ];
-
-        c = [
-          "yank_to_clipboard"
-          "delete_selection"
-        ];
-
-        y = ["yank_to_clipboard"];
-
-        R = "redo";
-
-        space.space = {
-          b = ":sh git blame -L %{cursor_line},%{cursor_line} %{buffer_name}";
-        };
-
-        C-n = "file_picker";
-        C-p = "file_picker";
-
-        A-c = {
-          u = ":pipe ccase -t upper";
-          l = ":pipe ccase -t lower";
-          t = ":pipe ccase -t title";
-          c = ":pipe ccase -t camel";
-          p = ":pipe ccase -t pascal";
-          s = ":pipe ccase -t snake";
-          k = ":pipe ccase -t kebab";
-        };
-
-        g.D = "no_op";
-        space.i = ":toggle-option lsp.display-inlay-hints";
-
-        g.d = {
-          c = ["goto_definition"];
-          v = [
-            "vsplit"
-            "jump_view_up"
-            "goto_definition"
-          ];
-          h = [
-            "hsplit"
-            "jump_view_up"
-            "goto_definition"
-          ];
-        };
-      };
-      keys.insert = {
-        j.k = "normal_mode";
-        C-c = "toggle_comments";
-        C-l = "move_char_right";
-        C-h = "move_char_left";
-        C-k = "move_line_up";
-        C-j = "move_line_down";
-        up = "no_op";
-        down = "no_op";
-        left = "no_op";
-        right = "no_op";
-        pageup = "no_op";
-        pagedown = "no_op";
-        home = "no_op";
-        end = "no_op";
-      };
-
-      keys.select = {
-        A-c = {
-          u = ":pipe ccase -t upper";
-          l = ":pipe ccase -t lower";
-          t = ":pipe ccase -t title";
-          c = ":pipe ccase -t camel";
-          p = ":pipe ccase -t pascal";
-          s = ":pipe ccase -t snake";
-          k = ":pipe ccase -t kebab";
-        };
-        y = ["yank_to_clipboard"];
-        C-k = [
-          "extend_to_line_bounds"
-          "delete_selection"
-          "move_line_up"
-          "paste_clipboard_before"
-        ];
-
-        C-j = [
-          "extend_to_line_bounds"
-          "delete_selection"
-          "paste_clipboard_after"
-        ];
-
-        p = [
-          "delete_selection"
-          "paste_clipboard_before"
-        ];
-
-        c = [
-          "yank_to_clipboard"
-          "delete_selection"
-        ];
-
-        d = ["delete_selection"];
-
-        A-q = [":reflow"];
-
-        g.f = {
-          c = ["goto_file"];
-          v = [
-            "vsplit"
-            "jump_view_up"
-            "goto_file"
+          V = [
+            "select_mode"
+            "extend_to_line_bounds"
           ];
 
-          h = [
-            "hsplit"
-            "jump_view_up"
-            "goto_file"
+          C-k = [
+            "extend_to_line_bounds"
+            "delete_selection"
+            "move_line_up"
+            "paste_clipboard_before"
           ];
+
+          D = [
+            "extend_to_line_bounds"
+            "delete_selection"
+          ];
+
+          C-j = [
+            "extend_to_line_bounds"
+            "delete_selection"
+            "paste_clipboard_after"
+          ];
+          A-q = [ ":reflow" ];
+          C-y = [ "scroll_up" ];
+          C-e = [ "scroll_down" ];
+
+          p = [
+            "delete_selection"
+            "paste_clipboard_before"
+          ];
+
+          c = [
+            "yank_to_clipboard"
+            "delete_selection"
+          ];
+
+          y = [ "yank_to_clipboard" ];
+
+          R = "redo";
+
+          C-n = "file_picker";
+          C-p = "file_picker";
+
+          A-c = {
+            u = ":pipe ccase -t upper";
+            l = ":pipe ccase -t lower";
+            t = ":pipe ccase -t title";
+            c = ":pipe ccase -t camel";
+            p = ":pipe ccase -t pascal";
+            s = ":pipe ccase -t snake";
+            k = ":pipe ccase -t kebab";
+          };
+        };
+        insert = {
+          j.k = "normal_mode";
+          C-c = "toggle_comments";
+          C-l = "move_char_right";
+          C-h = "move_char_left";
+          C-k = "move_line_up";
+          C-j = "move_line_down";
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+          pageup = "no_op";
+          pagedown = "no_op";
+          home = "no_op";
+          end = "no_op";
+        };
+        select = {
+          A-c = {
+            u = ":pipe ccase -t upper";
+            l = ":pipe ccase -t lower";
+            t = ":pipe ccase -t title";
+            c = ":pipe ccase -t camel";
+            p = ":pipe ccase -t pascal";
+            s = ":pipe ccase -t snake";
+            k = ":pipe ccase -t kebab";
+          };
+          y = [ "yank_to_clipboard" ];
+          C-k = [
+            "extend_to_line_bounds"
+            "delete_selection"
+            "move_line_up"
+            "paste_clipboard_before"
+          ];
+
+          C-j = [
+            "extend_to_line_bounds"
+            "delete_selection"
+            "paste_clipboard_after"
+          ];
+
+          p = [
+            "delete_selection"
+            "paste_clipboard_before"
+          ];
+
+          c = [
+            "yank_to_clipboard"
+            "delete_selection"
+          ];
+
+          d = [ "delete_selection" ];
+
+          A-q = [ ":reflow" ];
+
+          g.f = {
+            c = [ "goto_file" ];
+            v = [
+              "vsplit"
+              "jump_view_up"
+              "goto_file"
+            ];
+
+            h = [
+              "hsplit"
+              "jump_view_up"
+              "goto_file"
+            ];
+          };
         };
       };
     };
